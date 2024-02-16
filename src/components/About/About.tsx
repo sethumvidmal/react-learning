82e147d6e84ab2e7d5805fe768f57e58e4381bc3
Sethum Vidmal <sethumthabrew@gmail.com>
2024-02-16 13:08:28 +0530

    Use link

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function About() {
     return (
         <div>
             <h1>About page works!</h1>
+            <Link to="/">Go to Homepage</Link> <br />
+            <Link to="/contact">Go to contact Page</Link>
         </div>
     )
 }
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Contact(){
     return (
         <div>
             <h1>Contact page works!</h1>
+            <Link to="/">Go to Hompage</Link> <br />
+            <Link to="/about">Go to About Page</Link>
         </div>
     )
 }
